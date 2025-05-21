f7678a55215147629203763739dbf2a05f3f1c0f
Cristofer Miranda <cristofer@integritic.cl>
2025-05-21 17:00:26 -0400

    funcionando con cpanel en laravel_app

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginRegisterController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::controller(LoginRegisterController::class)->group(function() {
     Route::get('/register', 'register')->name('register');
@@ -13,4 +14,17 @@ Route::controller(LoginRegisterController::class)->group(function() {
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/clear-laravel-11-caches', function () {
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    Artisan::call('cache:clear'); // Caché general de la aplicación
+    Artisan::call('event:clear'); // Si usas eventos cacheados
+    // En Laravel 11, 'optimize:clear' agrupa muchas de estas:
+    // Artisan::call('optimize:clear'); // Deshace config:cache, route:cache, view:cache, event:cache
+
+    // Para estar seguros, ejecutar los individuales puede ser mejor al depurar
+    return "Laravel 11 Caches (config, view, route, app cache, event) have been cleared!";
 });
