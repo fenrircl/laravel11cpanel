1e342a637aa70d8df0621e6cec695989cf6a0171
Cristofer Miranda <cristofer.miranda@gmail.com>
2025-09-14 15:11:31 -0300

    test api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,30 @@
 <?php
-use App\Http\Controllers\FacturasController;
-use App\Http\Controllers\BackupController;
-// ... otras rutas
 
-Route::resource('facturas', FacturasController::class);
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes de prueba
+|--------------------------------------------------------------------------
+| Aquí puedes definir rutas simples para comprobar que /api funciona.
+| Las rutas estarán disponibles en https://tudominio.com/api/...
+|
+*/
+
+// Ruta de prueba básica
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong 🏓']);
+});
+
+// Ruta con parámetros
+Route::get('/saludo/{nombre}', function ($nombre) {
+    return response()->json(['saludo' => "Hola, $nombre 👋"]);
+});
+
+// Ruta POST para recibir datos
+Route::post('/echo', function (Request $request) {
+    return response()->json([
+        'recibido' => $request->all()
+    ]);
+});
